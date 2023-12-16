c68f27088f697763a64f487fea6dcf306048a2e6
Andrii Horlov <andreygorlovv@gmail.com>
2023-12-16 11:07:18 +0000

    Add scripts (Hud, Player, Enums)

---
+++ Assets/TestShooter/Hud/HudController.cs
@@ -0,0 +1,17 @@
+﻿using TestShooter.Data;
+using TMPro;
+using UnityEngine;
+
+namespace TestShooter.Hud
+{
+    public class HudController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _movementState;
+
+        public void SetState(Enums.MovementState movementState)
+        {
+            _movementState.text = movementState.ToString();
+            _movementState.color = Enums.MovementStatesDictionary[movementState];
+        }
+    }
+}
