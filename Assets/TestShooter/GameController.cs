6ccb1bcdcfd7682bbe692f3a3c227866804154a1
Andrii Horlov <andreygorlovv@gmail.com>
2023-12-19 19:03:52 +0000

    Add icon, add readme file, add possibility to quit the game

--- Assets/TestShooter/GameController.cs
+++ Assets/TestShooter/GameController.cs
@@ -7,15 +7,51 @@ namespace TestShooter
 {
     public class GameController : MonoBehaviour
     {
+        private const float EscapeTime = 8f;
+
         [SerializeField] private PlayerController _playerController;
         [SerializeField] private HudController _hudController;
         [SerializeField] private BonusController _bonusController;
+
+        private float _timer;
+        private bool _isEscapePressed;
 
         private void Start()
         {
             Cursor.lockState = CursorLockMode.Locked;
             _playerController.Init(_hudController);
             _bonusController.Init(_playerController, _hudController);
+            _hudController.HideQuitAlert();
+        }
+
+        private void LateUpdate()
+        {
+            if (_isEscapePressed)
+            {
+                _timer += Time.deltaTime;
+                if (_timer > EscapeTime)
+                {
+                    Debug.Log($"Time out.");
+                    _isEscapePressed = false;
+                }
+            }
+
+            if (!Input.GetKeyDown(KeyCode.Escape))
+            {
+                return;
+            }
+
+            if (!_isEscapePressed)
+            {
+                _hudController.ShowQuitAlert();
+                _isEscapePressed = true;
+                return;
+            }
+
+            if (_timer <= EscapeTime)
+            {
+                Application.Quit();
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+# CorPower VR (CPO VR - Game)
+![Screenshot](GameScreenshot.png)
+
+### Info
+Current project created in Unity 2022.3.2f1
+[Download here](https://github.com/FidgHorlov/ShooterTest/releases/tag/Release)
+
+### Useful information
+* Movement: **WASD** or **Arrows**
+* Jump: **Space**;
+* Quit Game: **Escape** two times;
+* Fast weapon shooting: **Left mouse button**
+* Hard weapon shooting: **Right mouse button**
+
+---
+* Bots aren't moving. 
+* The elevator works when you enter it.
+* To flight - enter the Upgrade cylinder on the second floor.
+* Flight not possible out of fixed room size.
+* Bonus appears in a random location in the room each random time (from 0 to 15 seconds).
+* Bonus live only for 10 seconds (if you don't catch it - wait for another one)
+* Movement increased for 4 seconds.
+* Damage increased for all weapons to the end of the game.
+
+© Andrii Horlov
